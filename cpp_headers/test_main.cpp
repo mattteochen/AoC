fc56b7b1e1fdcfb766420daf760be5699baa5837
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2021-11-27 03:19:10 +0100

    update comment

--- cpp_headers/test_main.cpp
+++ cpp_headers/test_main.cpp
@@ -16,7 +16,7 @@ void solve()
     ss line;
     ifstream input_file("text.txt");
     
-    /* declare the parsing object list, the library is intended to declare an obj for every line */
+    /* declare the parsing object list, the library is intended to declare an obj for every line or one obj for all the input (i.e. number list) */
     vector<input_parsing::Input_obj> my_input;
 
     /* declare the custom parser object */
