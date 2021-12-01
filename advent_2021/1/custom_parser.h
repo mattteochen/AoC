93e40cef94057c4f90e4a1e9bd536be417d4627c
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2021-12-01 09:00:28 +0100

    day1

---
+++ advent_2021/1/custom_parser.h
@@ -0,0 +1,26 @@
+#include "../../cpp_headers/mc.h"
+
+namespace N_custom_parsing
+{
+    /* define the custom parse class */
+    class Parse_standard : public N_input_parsing::Parse_Base
+    {
+    public:
+        /* This is a parse example */
+        void parse(ss &line, vector<N_input_parsing::Input_obj> &my_input);
+    };
+
+    class Parse_day16_2020 : public N_input_parsing::Parse_Base
+    {
+    public:
+        /* This is a parse example, note, this show only the firts part of the input */
+        void parse(ss &line, vector<char> &separators, vector<N_input_parsing::Input_obj> &my_input, ii &space_counters);
+        void get_name(ss &line, char &sep, ss &destination);
+        void get_range(ss &line, char &sep, v_ii &range_vec);
+    };
+        
+    /* parsing helper functions */
+    bool is_number(char &c);
+    bool is_uppercase_letter(char &c);
+    bool is_lowercase_letter(char &c);
+}
---
+++ advent_2021/1/run.cpp
@@ -0,0 +1,100 @@
+#include "custom_parser.h"
+
+void solve();
+void part_1(vector<N_input_parsing::Input_obj> &my_input);
+void part_2(vector<N_input_parsing::Input_obj> &my_input);
+
+int main()
+{
+    solve();
+    return 1;
+}
+
+void solve()
+{
+    ss line;
+    ifstream input_file("text.txt");
+    
+    /* declare the parsing object list, the library is intended to declare an obj for every line or one obj for all the input (i.e. number list) */
+    vector<N_input_parsing::Input_obj> my_input;
+
+    /* declare the custom parser object */
+    N_custom_parsing::Parse_standard myParser; 
+
+    while(getline(input_file, line))
+    {
+        myParser.parse(line, my_input);
+    }
+
+//    for (auto &ob : my_input)
+//    {
+//        cout << ob.getIntId() << endl;
+//    }
+
+    part_1(my_input);
+    part_2(my_input);
+}
+
+void part_2(vector<N_input_parsing::Input_obj> &my_input)
+{
+    int ans = 0;
+    int prev = 0;
+    int f = 1;
+    int counter = 0;
+    int sum = 0;
+
+    for (int j = 0; j < my_input.size(); j++)
+    {
+        N_input_parsing::Input_obj ob = my_input[j];
+        
+        if (counter < 3)
+        {
+            sum += ob.getIntId();
+            counter ++;
+        }
+        if (counter == 3 && f)
+        {
+            prev = sum;
+            sum = 0;
+            j -= 2;
+            counter = 0;
+            f = 0;
+        }
+        else if (counter == 3 && !f)
+        {
+            j -= 2;
+            counter = 0;
+            if (sum > prev) ans ++;
+            prev = sum;
+            sum = 0;
+        }
+    }
+
+    if (sum > prev) ans ++;
+
+    cout << "part 2: " << ans << endl;
+}
+
+void part_1(vector<N_input_parsing::Input_obj> &my_input)
+{
+    int ans = 0;
+    int prev = 0;
+    int f = 1;
+
+    for (auto &ob : my_input)
+    {
+        if (f)
+        {
+            prev = ob.getIntId();
+            f = 0;
+        }
+        else
+        {
+            if (ob.getIntId() > prev) ans ++;
+        }
+        prev = ob.getIntId();
+    }
+
+    cout << "part 1: " << ans << endl;
+}
+
