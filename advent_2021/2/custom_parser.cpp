f668559a5a45aa0044854adf0367607ec51a83f0
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2021-12-02 08:24:40 +0100

    add 2

---
+++ advent_2021/2/custom_parser.cpp
@@ -0,0 +1,121 @@
+#include "custom_parser.h"
+
+bool N_custom_parsing::is_number(char &c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+bool N_custom_parsing::is_uppercase_letter(char &c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
+
+bool N_custom_parsing::is_lowercase_letter(char &c)
+{
+    return (c >= 'a' && c <= 'z');
+}
+
+void N_custom_parsing::Parse_standard::parse_word_plus_num(ss &line, vector<N_input_parsing::Input_obj> &my_input)
+{
+    N_input_parsing::Input_obj *parsing_obj = new N_input_parsing::Input_obj();
+    ii i = 0;
+    ss s_id = "";
+    ii i_id;
+
+    while (i < line.length() && line[i] != ' ')
+    {
+        s_id += line[i ++];
+    }
+    i ++;
+    parsing_obj -> N_input_parsing::Input_obj::SetID(s_id);
+    s_id.clear();
+    while (i < line.length())
+    {
+        s_id += line[i ++];
+    }
+
+    i_id = stoi(s_id, nullptr, 0);
+    
+    parsing_obj -> N_input_parsing::Input_obj::SetID(i_id);
+
+    my_input.push_back(*parsing_obj);
+
+    free(parsing_obj);
+}
+
+void N_custom_parsing::Parse_standard::parse(ss &line, vector<N_input_parsing::Input_obj> &my_input)
+{
+    N_input_parsing::Input_obj *parsing_obj = new N_input_parsing::Input_obj();
+    ii num = stoi(line, nullptr, 0);
+    
+    parsing_obj -> N_input_parsing::Input_obj::SetID(num);
+
+    my_input.push_back(*parsing_obj);
+
+    free(parsing_obj);
+}
+
+void N_custom_parsing::Parse_day16_2020::get_name(ss &line, char &sep, ss &destination)
+{
+    ii i = 0;
+
+    while (i < line.length() && line[i] != sep)
+    {
+        destination += line[i++];
+    }
+}
+
+void N_custom_parsing::Parse_day16_2020::get_range(ss &line, char &sep, v_ii &range_vec)
+{
+    ii i = 0;
+    ss tmp_string = "";
+
+    while (i < line.length() && !is_number(line[i])) {i++;}
+    
+    while (i < line.length())
+    {
+        if (!is_number(line[i]))
+        {
+            i ++;
+            continue;
+        }
+        while (i < line.length() && is_number(line[i]))
+        {
+            tmp_string += line[i ++];
+        }
+        ii num = stoi(tmp_string, nullptr, 0);
+        range_vec.push_back(num);
+        tmp_string.clear();
+        i++;
+    }
+
+}
+
+void N_custom_parsing::Parse_day16_2020::parse(ss &line, vector<char> &separators, vector<N_input_parsing::Input_obj> &my_input, ii &space_counters)
+{
+    /* check if is empty line */
+    if (line.length() < 1) 
+    {
+        space_counters ++;
+        return;
+    }
+
+    switch(space_counters)
+    {
+        case 0:
+            ss tag = "";
+            v_ii range_vec;
+            N_input_parsing::Input_obj obj;
+
+            N_custom_parsing::Parse_day16_2020::get_name(line, separators[0], tag);
+            N_custom_parsing::Parse_day16_2020::get_range(line, separators[1], range_vec);
+
+            obj.SetMatch(tag, range_vec);
+            my_input.push_back(obj);
+            break;
+    }
+}
+
+
+
+
---
+++ advent_2021/2/run.cpp
@@ -0,0 +1,89 @@
+#include "custom_parser.h"
+
+void solve();
+void part_1(vector<N_input_parsing::Input_obj> &my_input);
+void part_2(vector<N_input_parsing::Input_obj> &my_input);
+
+int main()
+{
+    solve();
+    return 1;
+}
+
+void solve()
+{
+    ss line;
+    ifstream input_file("text.txt");
+    
+    /* declare the parsing object list, the library is intended to declare an obj for every line or one obj for all the input (i.e. number list) */
+    vector<N_input_parsing::Input_obj> my_input;
+
+    /* declare the custom parser object */
+    N_custom_parsing::Parse_standard myParser; 
+
+    while(getline(input_file, line))
+    {
+        myParser.parse_word_plus_num(line, my_input);
+    }
+
+//    for (auto &ob : my_input)
+//    {
+//        cout << ob.getStringID() << "  ";
+//        cout << ob.getIntId() << endl;
+//    }
+
+    part_1(my_input);
+    part_2(my_input);
+}
+
+void part_1(vector<N_input_parsing::Input_obj> &my_input)
+{
+    ii f = 0, d = 0;
+    
+    for (int j = 0; j < my_input.size(); j++)
+    {
+        N_input_parsing::Input_obj this_obj = my_input[j];
+        if (this_obj.getStringID() == "forward")
+        {
+            f += this_obj.getIntId();
+        }
+        else if (this_obj.getStringID() == "down")
+        {
+            d += this_obj.getIntId();
+        }
+        else if (this_obj.getStringID() == "up")
+        {
+            d -= this_obj.getIntId();
+        }
+        else cout << "ERR\n";
+    }
+
+    cout << "part 1: " << f * d << endl;
+}
+
+void part_2(vector<N_input_parsing::Input_obj> &my_input)
+{
+    ii f = 0, d = 0, a = 0;
+    
+    for (int j = 0; j < my_input.size(); j++)
+    {
+        N_input_parsing::Input_obj this_obj = my_input[j];
+        if (this_obj.getStringID() == "forward")
+        {
+            f += this_obj.getIntId();
+            d += (a * this_obj.getIntId());
+        }
+        else if (this_obj.getStringID() == "down")
+        {
+            a += this_obj.getIntId();
+        }
+        else if (this_obj.getStringID() == "up")
+        {
+            a -= this_obj.getIntId();
+        }
+        else cout << "ERR\n";
+    }
+
+    cout << "part 2: " << f * d << endl;
+}
+
